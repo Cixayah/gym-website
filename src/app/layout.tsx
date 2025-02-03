c71e5c7fd55fd225c47dd84dcf4053d7ef60d201
Cix <gabrielviniciodacosta@gmail.com>
2025-02-03 16:24:20 -0300

    #

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,28 @@
-import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import WhatsappFloatingButton from './components/WhatsappFloatingButton'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import WhatsappFloatingButton from './components/WhatsappFloatingButton';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Academia Extreme Fitness',
   description: 'Sua academia para resultados reais',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        {/* Se você quiser usar PNG, use o seguinte: */}
+        {/* <link rel="icon" href="/favicon.png" type="image/png" /> */}
+      </head>
       <body className={inter.className}>
         <Navbar />
         <main>{children}</main>
@@ -25,5 +30,5 @@ export default function RootLayout({
         <WhatsappFloatingButton /> {/* Botão flutuante adicionado aqui */}
       </body>
     </html>
-  )
+  );
 }
